9d82dfb66f113bfb9c0cbfb777d346f13c72c697
Rahul Bansal <bansalrahul7411@gmail.com>
2026-05-02 15:32:57 +0530

    trigger pipeline

--- terraform/cloudtrail.tf
+++ terraform/cloudtrail.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudtrail" "trail" {
+resource "aws_cloudtrail" "trail-v3" {
     name = "IAM_Audit-Trail"
     s3_bucket_name = aws_s3_bucket.trail_bucket.id
     include_global_service_events = true
